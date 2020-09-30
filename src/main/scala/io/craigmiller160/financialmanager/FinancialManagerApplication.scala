2afa9b813a7f51b7c6faa8937e75b660599a2fc2
Craig Miller <craigmiller160@gmail.com>
2020-09-29 20:05:59 -0400

    Code cleanup

--- src/main/scala/io/craigmiller160/financialmanager/FinancialManagerApplication.scala
+++ src/main/scala/io/craigmiller160/financialmanager/FinancialManagerApplication.scala
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class FinancialManagerApplication {
-  println("Working") // TODO delete this
-}
+class FinancialManagerApplication
 object FinancialManagerApplication extends App {
   SpringApplication.run(classOf[FinancialManagerApplication])
 }
