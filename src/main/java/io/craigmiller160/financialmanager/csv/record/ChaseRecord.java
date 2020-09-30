64838af9aea9d0241b612ddb1c5d5ad5f59f9a6c
Craig Miller <craigmiller160@gmail.com>
2020-09-30 13:49:08 -0400

    Fixed the handling of the amount when creating TransactionRecord

--- src/main/java/io/craigmiller160/financialmanager/csv/record/ChaseRecord.java
+++ src/main/java/io/craigmiller160/financialmanager/csv/record/ChaseRecord.java
@@ -35,7 +35,7 @@ public record ChaseRecord (
         return new TransactionRecord(
                 postingDate,
                 description,
-                amount
+                amount * -1
         );
     }
 
