a668b4da39f2a2f3618fb29f158bf06907839f1e
Craig Miller <craigmiller160@gmail.com>
2020-09-29 20:52:02 -0400

    Porting over POC of CSV parsing

---
+++ src/main/scala/io/craigmiller160/financialmanager/csv/parser/AbstractCsvParser.scala
@@ -0,0 +1,29 @@
+/*
+ *     Financial Manager API
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.craigmiller160.financialmanager.csv.parser
+
+abstract class AbstractCsvParser[R] {
+    protected def createRecord(rawRecord: String): R
+
+    def parse(csv: String): List[R] =
+        csv.split("\n")
+            .drop(1)
+            .map(createRecord)
+            .toList
+}
