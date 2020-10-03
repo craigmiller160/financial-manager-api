92a2e74b2ddca9b2b861bad8c5f67c90a2891632
Craig Miller <craigmiller160@gmail.com>
2020-10-03 09:45:36 -0400

    Removed TODO

--- src/test/java/io/craigmiller160/financialmanager/integration/ImportControllerIntegrationTest.java
+++ src/test/java/io/craigmiller160/financialmanager/integration/ImportControllerIntegrationTest.java
@@ -127,7 +127,6 @@ public class ImportControllerIntegrationTest {
                 hasProperty("userId", equalTo(JwtUtils.USERNAME)),
                 hasProperty("postDate", equalTo(postDate))
         ));
-        // TODO postDate
     }
 
     @Test
