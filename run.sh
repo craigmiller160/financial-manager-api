cce86b298e4727612c118155b31edbd96db07642
Craig Miller <craigmiller160@gmail.com>
2020-10-04 17:15:59 -0400

    Working on getting app to run in dev mode

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/dev/ -Dspring.profiles.active=dev"
+ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/dev/ -Dspring.profiles.active=dev --enable-preview"
 
 mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS"
