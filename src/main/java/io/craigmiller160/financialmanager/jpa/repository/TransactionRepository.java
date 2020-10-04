a53432cc29aeaf13086f0ef2cae919b436049189
Craig Miller <craigmiller160@gmail.com>
2020-10-04 12:00:57 -0400

    Wrote search query in repository

--- src/main/java/io/craigmiller160/financialmanager/jpa/repository/TransactionRepository.java
+++ src/main/java/io/craigmiller160/financialmanager/jpa/repository/TransactionRepository.java
@@ -19,8 +19,28 @@
 package io.craigmiller160.financialmanager.jpa.repository;
 
 import io.craigmiller160.financialmanager.jpa.entity.Transaction;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction,Long> { }
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+
+    @Query("""
+    SELECT t
+    FROM Transaction t
+    WHERE t.postDate >= :startDate
+    AND t.postDate <= :endDate
+    AND (:categoryIds IS NULL OR t.categoryId IN (:categoryIds))
+    """)
+    Page<Transaction> searchForTransactions(
+            @Param("startDate") final LocalDate startDate,
+            @Param("endDate") final LocalDate endDate,
+            @Param("categoryIds") final List<Long> categoryIds);
+
+}
