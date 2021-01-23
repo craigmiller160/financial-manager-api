b2b11c697c3624fea293fe699d7a1ffd2e830aa9
Craig Miller <craigmiller160@gmail.com>
2021-01-23 09:53:51 -0500

    Feature/pf 28 (#3)
    * Updated a lot of properties
    
    * Bumped versions
    
    * Fixed dependency name
    
    * Updated db credentials
    
    * Bumped spring version
    
    * Fixed startup bugs
    
    * Tiny configuration tweak

--- src/main/java/io/craigmiller160/financialmanager/config/WebSecurityConfig.java
+++ src/main/java/io/craigmiller160/financialmanager/config/WebSecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -55,6 +56,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .apply(jwtValidationFilterConfigurer)
+                .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 .and()
                 .exceptionHandling().authenticationEntryPoint(authEntryPoint);
 
